c53b0b8efc325bb051a5c94b3c1aa4a10be39d07
梁举恩 <3103421940@qq.com>
2023-04-17 18:47:37 +0800

    解决动态路由缓存不刷新页面问题

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,39 +1,43 @@
 <template>
-    <Header/>
-    <main>
-        <router-view v-slot="{ Component }">
-            <transition name="view-move" mode="out-in" appear>
-                <component :is="Component" />
-            </transition>
-        </router-view>
-    </main>
-    <GoTop/>
-    <Footer/>
+  <Header />
+  <main>
+    <router-view v-slot="{ Component }">
+      <transition name="view-move" mode="out-in" appear>
+        <component :is="Component" :key="route.path" />
+      </transition>
+    </router-view>
+  </main>
+  <GoTop />
+  <Footer />
 </template>
 
 <script setup lang="ts">
 import Header from '../layout/Header.vue'
 import Footer from '../layout/Footer.vue'
 import GoTop from '../components/GoTop.vue'
+import { useRoute } from 'vue-router'
+const route = useRoute()
 </script>
 
 <style>
-
 /* 页面跳转动画 */
 .view-move-leave-active {
-    animation: bounce-in .4s reverse;
+  animation: bounce-in .4s reverse;
+}
+
+.view-move-enter-active {
+  animation: bounce-in .3s;
+}
+
+@keyframes bounce-in {
+  0% {
+    opacity: 0;
+    transform: translateY(10px);
   }
-  .view-move-enter-active {
-    animation: bounce-in .3s;
+
+  100% {
+    opacity: 1;
+    transform: translateY(0);
   }
-  @keyframes bounce-in {
-    0% {
-      opacity: 0;
-      transform: translateY(10px);
-    }
-    100% {
-      opacity: 1;
-      transform: translateY(0);
-    }
-  }
+}
 </style>
